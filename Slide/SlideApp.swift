b9d325b7bb9b551d2f6ce6206ee24315d6fdb6ef
Kanop Sutharomna <78390700+arckanop@users.noreply.github.com>
2026-06-22 20:37:08 +0700

    Initial Commit

---
+++ Slide/SlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideApp.swift
+//  Slide
+//
+//  Created by Kanop Sutharomna on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
